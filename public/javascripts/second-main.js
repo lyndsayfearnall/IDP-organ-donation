188ccec262a3f1a218e30d1053c0a996f04ae1a7
Haley <h_ostrander@outlook.com>
2018-03-29 17:07:49 -0400

    animated hamburger menu for mobile

--- public/javascripts/second-main.js
+++ public/javascripts/second-main.js
@@ -3,6 +3,9 @@
 
 var videoToggler = document.querySelectorAll('.video-button');
 var video = document.querySelector('.full-video-wrapper');
+var menuToggler = document.querySelector('.ham-menu-wrapper');
+var menu = document.querySelector('.slide-nav-wrapper');
+var closeMenu = document.querySelector('.hamburger');
 
 function toggleVideo() {
 	console.log('SUP');
@@ -12,5 +15,24 @@ function toggleVideo() {
 videoToggler.forEach(function() {
 	this.addEventListener('click', toggleVideo, false);
 }); 	
+
+function toggleMenu() {
+	console.log('menu toggle being fondled')
+	menu.classList.toggle('nav-wrapper-active');
+	closeMenu.classList.toggle('is-active');
+	// menu.classList.add('slideInRight');
+	// stopScroll.classList.toggle('stopScroll')
+	// hamAnimation.classList.toggle('open-menu');
+}
+
+// navText.forEach(function(e, i) {
+// 	navText[i].addEventListener('click', function(e) {
+// 		e.preventDefault();
+// 		smoothScroll(navText[i]);
+// 		toggleMenu();
+// 	}, false);
+// });
+
+menuToggler.addEventListener('click', toggleMenu, false);
 
 // videoToggler.addEventListener('click', toggleVideo, false);
