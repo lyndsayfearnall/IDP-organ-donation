92b774b652e85ae9abcf69061ebc2cfd7e27b4a2
Haley <h_ostrander@outlook.com>
2018-03-29 21:38:03 -0400

    fixed video style issue and some minor styling fixes as well

--- public/javascripts/second-main.js
+++ public/javascripts/second-main.js
@@ -1,38 +1,48 @@
 
-// ***** Video Animation ***** //
-
 var videoToggler = document.querySelectorAll('.video-button');
 var video = document.querySelector('.full-video-wrapper');
+
 var menuToggler = document.querySelector('.ham-menu-wrapper');
 var menu = document.querySelector('.slide-nav-wrapper');
 var closeMenu = document.querySelector('.hamburger');
 
+var navText = document.querySelectorAll('.nav-scroll');
+var stopScroll = document.querySelector('body');
+
+
+
 function toggleVideo() {
 	console.log('SUP');
 	video.classList.toggle('full-video-active');
+	// stopScroll.classList.toggle('stopScroll')
 }
 
-videoToggler.forEach(function() {
-	this.addEventListener('click', toggleVideo, false);
+navText.forEach(function(e, i) {
+	navText[i].addEventListener('click', function(e) {
+		e.preventDefault();
+		smoothScroll(navText[i]);
+		toggleMenu();
+	}, false);
+});
+
+videoToggler.forEach(function(e, i) {
+	videoToggler[i].addEventListener('click', toggleVideo, false);
 }); 	
 
 function toggleMenu() {
 	console.log('menu toggle being fondled')
 	menu.classList.toggle('nav-wrapper-active');
 	closeMenu.classList.toggle('is-active');
-	// menu.classList.add('slideInRight');
-	// stopScroll.classList.toggle('stopScroll')
-	// hamAnimation.classList.toggle('open-menu');
 }
-
-// navText.forEach(function(e, i) {
-// 	navText[i].addEventListener('click', function(e) {
-// 		e.preventDefault();
-// 		smoothScroll(navText[i]);
-// 		toggleMenu();
-// 	}, false);
-// });
 
 menuToggler.addEventListener('click', toggleMenu, false);
 
-// videoToggler.addEventListener('click', toggleVideo, false);
+function smoothScroll(e) {
+	console.log(e);
+	var elementToScroll = e.getAttribute("href");
+	console.log(elementToScroll);
+	document.querySelector(elementToScroll).scrollIntoView({ 
+	  behavior: 'smooth' 
+	});
+
+}
