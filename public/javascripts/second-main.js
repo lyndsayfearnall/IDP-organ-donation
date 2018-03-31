801cf4344c888b781da6e28dd1f7b58f3c046131
Haley <h_ostrander@outlook.com>
2018-03-31 17:18:40 -0400

    adding in working quiz, result page style is not finished yet

--- public/javascripts/second-main.js
+++ public/javascripts/second-main.js
@@ -9,13 +9,94 @@ var closeMenu = document.querySelector('.hamburger');
 var navText = document.querySelectorAll('.nav-scroll');
 var stopScroll = document.querySelector('body');
 
+var quizToggler = document.querySelectorAll('.quiz-toggle');
+var quiz = document.querySelector('.quiz-page-wrapper');
+
+var quizPage = document.querySelectorAll('.quiz-q-wrapper');
+var quizOption = document.querySelectorAll('.q-con');
 
 
+// ***** Functioning Quiz ***** //
+function checkAnswers(){
+	console.log('checking answers');
+
+	var answers = [];
+
+	for (var i = 1; i < 7; i++) {
+		var checkedAnswerItems = 0;
+		var answerItems = document.querySelectorAll('input[value="a'+i+'"]');
+		answerItems.forEach(function(e,index) {
+			if (answerItems[index].checked) {
+				checkedAnswerItems++;
+			}
+		});
+		answers.push(checkedAnswerItems);
+	}
+
+	var quizOutcome = document.querySelector(".quizOutcome");
+
+	console.log(Math.max(...answers));
+	var mostSelected = Math.max(...answers);
+
+	if (answers[0] === mostSelected || answers[1] === mostSelected) {
+		console.log('one of the top two is most');
+	}
+	else if (answers[2] === mostSelected || answers[3] === mostSelected) {
+		console.log('one of the middle two is most');
+	}
+	else if (answers[4] === mostSelected || answers[5] === mostSelected) {
+		console.log('one of the bottom two is most');
+
+	}
+}
+
+
+// ***** Cycling Quiz Pages ***** //
+
+var quizPageCount = 1;
+
+function toggleActiveOptions(e, i) {
+	this.parentElement.classList.remove('active');
+
+	if (quizPageCount < 5) {
+		this.parentElement.nextElementSibling.classList.add('active');
+		quizPageCount++;
+	}
+	else {
+		checkAnswers();
+	}
+}
+
+quizOption.forEach(function(e, i) {
+	quizOption[i].addEventListener('click', toggleActiveOptions, false);
+});
+
+
+// ***** Toggling Quiz ***** //
+
+function toggleQuiz() {
+	// console.log('quiz toggle being fondled')
+	quiz.classList.toggle('quiz-active');
+	// closeMenu.classList.toggle('is-active');
+}
+
+quizToggler.forEach(function(e, i) {
+	quizToggler[i].addEventListener('click', toggleQuiz, false);
+});
+
+
+//  ***** Toggling Video Slide ***** //
 function toggleVideo() {
 	console.log('SUP');
 	video.classList.toggle('full-video-active');
 	// stopScroll.classList.toggle('stopScroll')
 }
+
+videoToggler.forEach(function(e, i) {
+	videoToggler[i].addEventListener('click', toggleVideo, false);
+}); 
+
+// ***** Toggling Menu Slide and Smooth Scrolling ***** //
 
 navText.forEach(function(e, i) {
 	navText[i].addEventListener('click', function(e) {
@@ -23,14 +104,9 @@ navText.forEach(function(e, i) {
 		smoothScroll(navText[i]);
 		toggleMenu();
 	}, false);
-});
-
-videoToggler.forEach(function(e, i) {
-	videoToggler[i].addEventListener('click', toggleVideo, false);
-}); 	
+});	
 
 function toggleMenu() {
-	console.log('menu toggle being fondled')
 	menu.classList.toggle('nav-wrapper-active');
 	closeMenu.classList.toggle('is-active');
 }
